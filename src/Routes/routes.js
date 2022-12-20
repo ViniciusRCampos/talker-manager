bc17a6353149335aa3d5dbbd03243d7d87b821d7
ViniciusRCampos <vinicius_uo@hotmail.com>
2022-12-20 11:09:33 -0300

    feat: endpoint GET /talker/:id

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -9,6 +9,18 @@ router.get('/talker', async (_req, res) => {
     res.status(200).json(talkers);
 });
 
+router.get('/talker/:id', async (req, res) => {
+    const { id } = req.params;
+    const talkers = await getTalkers();
+    const talkerId = talkers.find((talker) => talker.id === +id);
+    if (talkerId) {
+       return res.status(200).json(talkerId);
+    }
+    return res.status(404).json(
+        { message: 'Pessoa palestrante não encontrada' },
+    );
+});
+
 module.exports = {
     router,
 };
